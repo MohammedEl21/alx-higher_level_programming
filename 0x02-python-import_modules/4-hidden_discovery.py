8c67074d4d67271be4d40503088368130e16c8c5
Mohammed <mme43@Njit.edu>
2023-02-17 21:25:32 -0800

    Updated

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    for str in dir(hidden_4):
-        if str[:2] != '__':
-            print(str)
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
+
